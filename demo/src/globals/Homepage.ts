71ab3d7216194557552122cd950a4e653f381b23
Paul Popus <popuspaul@gmail.com>
2023-04-12 17:58:13 +0700

    Add fields to homepage global

--- demo/src/globals/Homepage.ts
+++ demo/src/globals/Homepage.ts
@@ -2,7 +2,13 @@ import { GlobalConfig } from "payload/types";
 
 const Homepage: GlobalConfig = {
   slug: "homepage",
-  fields: [],
+  fields: [
+    {
+      name: "featuredPost",
+      type: "relationship",
+      relationTo: "posts",
+    },
+  ],
 };
 
 export default Homepage;
